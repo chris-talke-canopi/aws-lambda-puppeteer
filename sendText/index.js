464485fcd2897d0fcb23a403a503a202b0fe4fee
Christopher Talke <85919905+chris-talke-canopi@users.noreply.github.com>
2024-03-21 13:05:16 +1100

    Added another func

--- sendText/index.js
+++ sendText/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     const response = {
       statusCode: 200,
       body: {
-        message: "Hello from AWS and Github, Get Function"
+        message: "Hello from AWS and Github, Send Function"
       },
     }
     return response
