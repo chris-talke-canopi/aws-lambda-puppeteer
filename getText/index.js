c102fd35ecf1c5ebeca6de0719b5c22a2fe6f355
Christopher Talke <85919905+chris-talke-canopi@users.noreply.github.com>
2024-03-21 13:13:33 +1100

    puppeteer

--- getText/index.js
+++ getText/index.js
@@ -1,9 +1,23 @@
+const puppeteer = require('puppeteer');
+
 exports.handler = async (event) => {
+
+    const browser = await puppeteer.launch({ headless: true });
+    let page = await browser.newPage();
+
+    await page.goto('https://pptr.dev/');
+
+    const text = await page.evaluate( () => {
+        let text = document.querySelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div.col.docItemCol_VOVn > div > article > div.theme-doc-markdown.markdown > blockquote').textContent;
+        return text || `This didn't work`;
+    });
+
     const response = {
       statusCode: 200,
       body: {
-        message: "Hello from AWS and Github, Get Function"
+        message: text
       },
     }
+
     return response
   }
