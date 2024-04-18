fedfcf263ed99679459a2cf32386a038a8bdc2bd
Christopher Talke <85919905+chris-talke-canopi@users.noreply.github.com>
2024-04-18 10:52:48 +1000

    Working function with layer

--- getText/index.js
+++ getText/index.js
@@ -2,29 +2,22 @@ const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 
 exports.handler = async (event) => {
-
-  const browser = await puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(process.env.AWS_EXECUTION_ENV ? "/opt/nodejs/node_modules/@sparticuz/chromium/bin" : undefined),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-  });
-    let page = await browser.newPage();
-
-    await page.goto('https://pptr.dev/');
-
-    const text = await page.evaluate( () => {
-        let text = document.querySelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div.col.docItemCol_VOVn > div > article > div.theme-doc-markdown.markdown > blockquote').textContent;
-        return text || `This didn't work`;
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(
+            "https://canopi-chris-bucket.s3.ap-southeast-2.amazonaws.com/chromium-v112.0.2-pack.tar"
+        ),
+        headless: chromium.headless,
     });
 
-    const response = {
-      statusCode: 200,
-      body: {
-        message: text
-      },
-    }
+    const page = await browser.newPage();
+    await page.goto("https://github.com/Sparticuz/chromium");
+    const pageTitle = await page.title();
+    await browser.close();
 
-    return response
-  }
+    return {
+        status: 200,
+        message: pageTitle,
+    };
+};
--- sendText/index.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-      statusCode: 200,
-      body: {
-        message: "Hello from AWS and Github, Send Function"
-      },
-    }
-    return response
-  }
